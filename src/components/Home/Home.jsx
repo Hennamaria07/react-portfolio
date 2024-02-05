1a8dbaf4f1ff9b94cddeeab3845cddcfb46451fc
Henna Maria <hennamaria2001@gmail.com>
2024-02-05 19:26:43 +0530

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { useEffect} from "react";
 import Typed from 'typed.js';
+import {About} from "../About/About.jsx"
 import './home.css';
 import { Link } from 'react-router-dom';
-import { BD, FD, ML, Others } from './Cards.jsx';
 
 const Home = () => {
     useEffect(() => {
@@ -23,24 +23,6 @@ const Home = () => {
             };
         }
     }, []); 
-
-    const [DisplayPage, setDisplayPage] = useState(0);
-    const renderProjects = () => {
-        switch (DisplayPage) {
-            case 0:
-                // Display all projects
-                return <FD />;
-            case 1:
-                // Display web projects
-                return <BD />;
-            case 2:
-                return <ML />;
-            case 3:
-                return <Others />;
-            default:
-                return null;
-        }
-    };
     return (
         <>
             <div className='w-screen mt-16 md:mt-0 overflow-hidden home-wrapper bg-cyan-950'>
@@ -48,7 +30,7 @@ const Home = () => {
                 <div className="flex flex-row container-padding h-full md:px-10 items-center">
           <div className="md:w-1/2 md:pt-0">
             
-    <p className='text-zinc-300 sm:pt-14 pt-14 md:pt-32'>__________<span className='sm:inline hidden'>_______</span>_________What is she?</p>
+    <p className='text-zinc-300 sm:pt-14 pt-14 md:pt-32'>__________<span className='sm:inline hidden'>_______</span>_________Who is she?</p>
             <div className='heading-height'>
             <h1 className="text-5xl md:text-6xl text-center text-sky-300 font-bold">
               I'M A <span className="text text-sky-500"></span>
@@ -75,47 +57,8 @@ const Home = () => {
             </svg>
           </div>
         </div>
-        <div className="container-padding md:px-10 bg-cyan-950 w-screen min:h-screen overflow-hidden" style={{ paddingTop: "72px" }}>
-                <div className="row">
-                    <div className="col">
-                        <p className='text-red-100 md:text-lg text-md'>__________<span className='sm:inline hidden'>_______</span>_________What she has built?</p>
-                        <h1 className='pt-4 text-center lg:text-6xl md:text-5xl sm:text-4xl text-2xl text-gray-200 font-bold'>Her skill set spans across full-stack development</h1>
-                        <p className="paragraph text-md md:text-lg mt-1 text-center text-gray-200 lg:mt-4 md:pt-1">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate ut, ullam perferendis cupiditate incidunt, voluptatem cumque tempore rerum laboriosam maxime! Cum aperiam nemo culpa modi ipsam rem mollitia eligendi.
-                        </p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col buttons flex justify-center py-4">
-                        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 active:from-pink-500 active:to-orange-400" onClick={(e) => setDisplayPage(0)}>
-                            <span className="relative px-5 py-2.5 transition-all text-zinc-100 ease-in duration-75 bg-cyan-950 rounded-md group-hover:bg-opacity-0">
-                                FRONTEND
-                            </span>
-                        </button>
-                        <button className="relative inline-flex mx-5 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 active:from-pink-500 active:to-orange-400" onClick={(e) => setDisplayPage(1)}>
-                            <span className="relative px-5 py-2.5 transition-all text-zinc-100 ease-in duration-75 bg-cyan-950 rounded-md group-hover:bg-opacity-0">
-                                BACKEND
-                            </span>
-                        </button>
-                        <button className="relative inline-flex mx-5 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 active:from-pink-500 active:to-orange-400">
-                            <span className="relative px-5 py-2.5 transition-all text-zinc-100 ease-in duration-75 bg-cyan-950 rounded-md group-hover:bg-opacity-0" onClick={(e) => setDisplayPage(2)}>
-                                ML
-                            </span>
-                        </button>
-                        <button className="relative inline-flex mx-5 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 active:from-pink-500 active:to-orange-400">
-                            <span className="relative px-5 py-2.5 transition-all text-zinc-100 ease-in duration-75 bg-cyan-950 rounded-md group-hover:bg-opacity-0" onClick={(e) => setDisplayPage(3)}>
-                                OTHERS
-                            </span>
-                        </button>
-                    </div>
-                </div>
-                <div className="row">
-                {renderProjects()}
-                </div>
             </div>
-
-            </div>
-            {/* <About /> */}
+            <About />
         </>
     )
 }
